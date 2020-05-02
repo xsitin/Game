e4ce8885c62eeb4744381625f4aa81051c3667d2
xsitin <m9ckom@gmail.com>
2020-05-02 17:28:39 +0500

    created hero class

---
+++ Game/Model/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Model
+{
+    public class Hero:BaseCreature
+    {
+        public Hero(string name, int health, CreatureType type) : base(name, health, type)
+        { }
+
+        public List<Item> Inventory = new List<Item>();
+    }
+}
