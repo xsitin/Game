791c391531c06e4b0af51bf45f3d56c0426d00f6
AppleePie <thelongwaters@yandex.ru>
2020-05-03 21:31:57 +0500

    Update Hero.cs
    ^_^

--- Game/Model/Hero.cs
+++ Game/Model/Hero.cs
@@ -5,7 +5,9 @@ namespace Game.Model
     public class Hero:BaseCreature
     {
         public Hero(string name, int health, CreatureType type) : base(name, health, type)
-        { }
+        { 
+            //Ыы
+        }
 
         public List<Item> Inventory = new List<Item>();
     }
