e48c8e2ea3a557aad53c8263ae77b8581b5e3a1c
Loliconshik <mr.aloner@maul.ru>
2020-05-05 00:11:31 +0500

    DA v5
    Model Retry

---
+++ packages/Game/Model/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Game.Model
+{
+    public abstract class Item
+    {
+        public string Name { get; }
+        public Item(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public class ActiveItem : Item 
+    {
+        private readonly Action<BasicCreature> _action;
+
+        public ActiveItem(string name, Action<BasicCreature> action) : base(name)
+        {
+            _action = action;
+        }
+
+        public void Use(params BasicCreature[] targets)
+        {
+            foreach (var target in targets)
+                _action(target);
+        }
+    }
+}
